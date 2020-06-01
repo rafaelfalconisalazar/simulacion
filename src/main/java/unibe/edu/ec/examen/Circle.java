02983dba60ae09e168d43045d5660f4ab2f68141
rafael <r.falconi9030@gmail.com>
2020-06-01 10:31:25 -0500

    add geters and setters

--- src/main/java/unibe/edu/ec/examen/Circle.java
+++ src/main/java/unibe/edu/ec/examen/Circle.java
@@ -8,6 +8,14 @@ public class Circle {
 		super();
 		this.radius = radius;
 	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
 	
 	
 
